ab8f517fcdf0aa25db6835f72938e37bb17754b5
rabucejo <rogerhapay@gmail.com>
2023-03-03 20:13:43 +0800

    deleted right side of the login component

--- src/components/LoginComp.jsx
+++ src/components/LoginComp.jsx
@@ -81,61 +81,16 @@ const LoginComp = () => {
           </Card>
         </DivStyles>
 
-        <DivStyles>
-          <Card
-            sx={{
-              width: '300px',
-              height: '500px',
-              padding: '10px',
-              margin: '30px',
-              border: '1px solid black',
-            }}
-          >
-            <Typography sx={{ textAlign: 'center' }}>Login here</Typography>
-            <CardContent>
-              <Grid container spacing={2}>
-                <Grid xs={12} item>
-                  <Input
-                    startAdornment={
-                      <InputAdornment position='start'>
-                        <VscAccount />
-                      </InputAdornment>
-                    }
-                    variant='outlined'
-                    type='text'
-                    placeholder='Username'
-                    fullWidth
-                    required
-                  ></Input>
-                </Grid>
-                <Grid xs={12} item>
-                  <Input
-                    startAdornment={
-                      <InputAdornment position='start'>
-                        <RiLockPasswordLine />
-                      </InputAdornment>
-                    }
-                    variant='outlined'
-                    type='text'
-                    placeholder='Password'
-                    fullWidth
-                    required
-                  ></Input>
-                </Grid>
-                <Grid xs={12} item>
-                  <Button
-                    variant='contained'
-                    color='primary'
-                    type='submit'
-                    fullWidth
-                  >
-                    Login
-                  </Button>
-                </Grid>
-              </Grid>
-            </CardContent>
-          </Card>
-        </DivStyles>
+        <div
+          style={{
+            width: '300px',
+            height: '500px',
+            padding: '10px',
+            margin: '30px',
+          }}
+        >
+          
+        </div>
       </DivStyles>
     </>
   )
