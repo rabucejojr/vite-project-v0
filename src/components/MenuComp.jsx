1f1be705c7138f44b3d6a5e91dc680e2862194af
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-09 09:32:43 +0800

    fixed component name in menucomp

--- src/components/MenuComp.jsx
+++ src/components/MenuComp.jsx
@@ -2,28 +2,28 @@ import * as React from 'react'
 import { ThemeProvider } from '@mui/system'
 import { AppBar, Box, createTheme, CssBaseline, IconButton, styled, Toolbar } from '@mui/material'
 
-
-  const MenuComp = () => {
-    const customMenuTheme = createTheme({
-      palette: {
-        primary: {
-          main: '#121456',
-          contrastText: 'white',
-        },
+const MenuComp = () => {
+  const customMenuTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#121456',
+        contrastText: 'white',
       },
-    })
-    const MenuTheme = styled('div')(({ theme }) => ({
-      color: theme.palette.primary.contrastText,
-      backgroundColor: theme.palette.primary.main,
-      padding: theme.spacing(1),
-      borderRadius: theme.shape.borderRadius,
-    }))
-    return (
-      <>
-        <ThemeProvider theme={customMenuTheme}>
-          <MenuTheme>Styled div with theme</MenuTheme>
-        </ThemeProvider>
-      </>
-    )
-  }
- export default MenuComp
+    },
+  })
+  const MenuTheme = styled('div')(({ theme }) => ({
+    color: theme.palette.primary.contrastText,
+    backgroundColor: theme.palette.primary.main,
+    padding: theme.spacing(1),
+    borderRadius: theme.shape.borderRadius,
+  }))
+  
+  return (
+    <>
+      <ThemeProvider theme={customMenuTheme}>
+        <MenuTheme>Styled div with theme</MenuTheme>
+      </ThemeProvider>
+    </>
+  )
+}
+export default MenuComp
--- src/components/Styles.jsx
+++ src/components/Styles.jsx
@@ -4,9 +4,6 @@ import { styled } from '@mui/material'
 const DivStyles = styled('div')({
     display: 'flex', alignItems: 'center', justifyContent: 'space-around',
 })
-
-
-
-
 export default DivStyles
+
 
