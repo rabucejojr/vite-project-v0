b743cbcfecc08249f9c2f01f483fcf47c070ccb4
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-10 16:03:11 +0800

    fixed imports

--- src/components/MenuComp.jsx
+++ src/components/MenuComp.jsx
@@ -1,15 +1,6 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
+import { Box, Drawer, AppBar, CssBaseline, Toolbar, List, Typography, Divider, ListItem, ListItemButton, ListItemText } from '@mui/material/Box';
+
 
 const drawerWidth = 240;
 
