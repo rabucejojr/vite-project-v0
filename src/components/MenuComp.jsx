b3d2f6985233fee47461b89d8a1b5a69611796b0
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-10 14:25:04 +0800

    created a menu/dashboard page

--- src/components/MenuComp.jsx
+++ src/components/MenuComp.jsx
@@ -1,30 +1,76 @@
-import * as React from 'react'
-import { ThemeProvider } from '@mui/system'
-import { AppBar, Box, createTheme, CssBaseline, IconButton, styled, Toolbar } from '@mui/material'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText';
 
-const MenuComp = () => {
-  const customMenuTheme = createTheme({
-    palette: {
-      primary: {
-        main: '#121456',
-        contrastText: 'white',
-      },
-    },
-  })
-  const MenuTheme = styled('div')(({ theme }) => ({
-    color: theme.palette.primary.contrastText,
-    backgroundColor: theme.palette.primary.main,
-    padding: theme.spacing(1),
-    borderRadius: theme.shape.borderRadius,
-  }))
+const drawerWidth = 240;
 
+export default function MenuComp() {
   return (
-    <>
-      <ThemeProvider theme={customMenuTheme}>
-        <MenuTheme>Styled div with theme</MenuTheme>
-      </ThemeProvider>
-      
-    </>
-  )
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            PSTO Management System
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                {/* <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon> */}
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        {/* <Toolbar /> */}
+        
+      </Box>
+    </Box>
+  );
 }
-export default MenuComp
