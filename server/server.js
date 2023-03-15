2a35c8daa1d38175f9610c2863a2ecfab09e9cd3
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-15 10:50:20 +0800

    modified mongoose and atlas connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
-const express = requrie('express')
+const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
 
 
 app.use(express.json())
 
-app.listen(3001, ()=>{
-    console.log('Server running in port 3001...')
+mongoose.connect("mongodb+srv://rabucejo:<password>@management.lt4hoy8.mongodb.net/?retryWrites=true&w=majority",
+{
+    useNewUrlParser: true,
+});
+
+app.listen(port, ()=>{
+    console.log(`server running in port ${port}`)
 })
